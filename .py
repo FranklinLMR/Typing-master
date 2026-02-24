41f5b25b0c0ce4067a9276fd70969bbe5f81249c
Franklin <rosariosanchezfranklin@gmail.com>
2026-02-24 14:40:20 -0400

    Adding color and functionality
    Adding the functionality of detecting wrong/correct wording, changing the color when wrong/right

--- .py
+++ .py
@@ -40,7 +40,34 @@ else:
 for f in range(index,len(data)):
     typeob(f"{data[f]} ", 0.01)
 
+while True:
+        lol= input("\n")
+        if lol == data[index]:
+            index+=1
+            for x in range(0,100):
+                print("")
+            for l in range(0, index):
+                typeob(f"{data[l]}", 0.01, on_color="on_green")
+                typeob(f" ", 0)
+            for f in range(index,len(data)):
+                typeob(f"{data[f]} ", 0.01)
+        else:
+            for x in range(0,100):
+                print("")
+            if index != 0:
+                for l in range(0, index):
+                    typeob(f"{data[l]}", 0.01, on_color="on_green")
+                    typeob(f" ", 0)
+                
+                typeob(f"{data[index]}", 0.01, on_color="on_red")
+                typeob(" ",0)
 
+            else:
+                typeob(f"{data[index]}", 0.01, on_color="on_red")
+                typeob(f" ", 0)
+            for f in range(index+1,len(data)):
+                typeob(f"{data[f]} ", 0.01)
+    
 
 
 crr=time.time()
