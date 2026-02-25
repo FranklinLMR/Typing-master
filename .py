d485d511b9448e8081c4de5a62233839dbbb441f
Franklin <rosariosanchezfranklin@gmail.com>
2026-02-25 09:06:07 -0400

    Reverse accidental commits

--- .py
+++ .py
@@ -1,12 +1,14 @@
+import random
+import re
 import requests 
 import time 
 import sys
 import keyboard
-from termcolor import colored   
+from termcolor import colored, cprint   
 from time import sleep
 
 
-#Machine writting function with enter (also with color capabilities)
+#Machine writting function with enter
 
 def typeoa(text, retraso=0.1, color=None, on_color = None, attrs= None):
     for letra in text:
@@ -20,7 +22,7 @@ def typeoa(text, retraso=0.1, color=None, on_color = None, attrs= None):
     print()
 
 
-#Machine writting function without enter (also with color capabilities)
+#Machine writting function without enter
 
 def typeob(text, retraso=0.1, color=None, on_color = None, attrs= None):
     for letra in text:
@@ -41,93 +43,79 @@ for f in range(0,100):
 
 
 #Retrieving the API
-while True:
 
-    Mozambique= requests.get("https://random-word-api.herokuapp.com/word?number=25")
-
-
-
-    #General Index for everything we gonna retrieve from the API data
-
-    index= 0
+Mozambique= requests.get("https://random-word-api.herokuapp.com/word?number=25")
 
 
 
-    #Extracting the data of the API and verifying everything's working (User-Sever relationship)
-    if Mozambique.status_code == 200:
-        data= Mozambique.json()
-    else:
-        print("this is not working")
+#General Index for everything we gonna retrieve from the API data
+
+index= 0
 
 
 
-    #Round of words without input (first showcase)
-
-    typeob(f"{data[index]}", 0.01, color="yellow", on_color="on_dark_grey")
-    typeob(f" ", 0)
-    for f in range(index+1,len(data)):
-        typeob(f"{data[f]} ", 0.01)
-
+#Extracting the data of the API and verifying everything's working (User-Sever relationship)
+if Mozambique.status_code == 200:
+    data= Mozambique.json()
+else:
+    print("this is not working")
 
 
 
-    
-    #Starting a stopwatch
-    start_time= time.time()
+#Round of words without input (first showcase)
 
-    #Input, round of words with indications of validity
-    while True:
+typeob(f"{data[index]}", 0.01, color="yellow", on_color="on_dark_grey")
+typeob(f" ", 0)
+for f in range(index+1,len(data)):
+    typeob(f"{data[f]} ", 0.01)
+
+
+
+#Input, round of words with indications of validity
+
+try:
+    while index != 25:
             for x in range(0,10):
-                    print("")
-                
-            lol= input("\n")
+                print("")
             
-            if index !=24:
-                if lol == data[index]:
-                    index+=1
-                    for x in range(0,100):
-                        print("")
-                    for l in range(0, index):
-                        typeob(f"{data[l]}", 0, on_color="on_green")
-                        typeob(f" ", 0)
-                    typeob(f"{data[index]}", 0, color="yellow", on_color="on_dark_grey")
+            lol= input("\n")
+            if lol == data[index]:
+                index+=1
+                for x in range(0,100):
+                    print("")
+                for l in range(0, index):
+                    typeob(f"{data[l]}", 0.0001, on_color="on_green")
                     typeob(f" ", 0)
-                    for f in range(index+1,len(data)):
-                        typeob(f"{data[f]} ", 0)
-                else:
-                    for x in range(0,100):
-                        print("")
-                    if index != 0:
-                        for l in range(0, index):
-                            typeob(f"{data[l]}", 0, on_color="on_green")
-                            typeob(f" ", 0)
-                        
-                        typeob(f"{data[index]}", 0, on_color="on_red")
-                        typeob(" ",0)
-
-                    else:
-                        typeob(f"{data[index]}", 0, on_color="on_red")
-                        typeob(f" ", 0)
-                    for f in range(index+1,len(data)):
-                        typeob(f"{data[f]} ", 0)
+                typeob(f"{data[index]}", 0.01, color="yellow", on_color="on_dark_grey")
+                typeob(f" ", 0)
+                for f in range(index+1,len(data)):
+                    typeob(f"{data[f]} ", 0.001)
             else:
-                break
+                for x in range(0,100):
+                    print("")
+                if index != 0:
+                    for l in range(0, index):
+                        typeob(f"{data[l]}", 0.0001, on_color="on_green")
+                        typeob(f" ", 0)
+                    
+                    typeob(f"{data[index]}", 0.0001, on_color="on_red")
+                    typeob(" ",0)
 
-    #Stopping our stopwatch
-    
-    end_time = time.time()
-    for x in range(0,100):
-        print("")
-    for l in range(0, index):
-        typeob(f"{data[l]}", 0.0001, on_color="on_green")
-        typeob(f" ", 0)
-    #Gathering time lapsed
-    time_lapsed = end_time - start_time
-
-    #Final message
+                else:
+                    typeob(f"{data[index]}", 0.0001, on_color="on_red")
+                    typeob(f" ", 0)
+                for f in range(index+1,len(data)):
+                    typeob(f"{data[f]} ", 0.0001)
+        
+except:
     typeob("Well done", 0.05, color= "yellow")
     typeob("...",0.5)
     typeoa("You have succesfully completed the round of words in around:",0.05)
-    typeoa(f"{round(time_lapsed), 2} seconds")
 
 
+crr=time.time()
+
+print(crr)
+
+
+print(' '.join(data))
